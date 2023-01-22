6b66fa703971091034462f8bd67e1a50e7eeb3eb
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-22 12:10:38 -0300

    first commit, add web react base

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import Habit from './components/Habit';
+import './styles/global.css';
+
+function App() {
+
+  return (
+    <><Habit completed={3} /><Habit completed={7} /></>
+  )
+}
+
+export default App
