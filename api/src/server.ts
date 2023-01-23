e0062473f895b18292e9988630c20343a09285a3
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-22 22:38:51 -0300

    refactor routes to router

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,11 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import appRoutes from './routers/router';
 
 const PORT = process.env.PORT || '3001';
-const prisma = new PrismaClient();
 const app = Fastify();
 app.register(cors);
-
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany();
-  return habits
-});
-
+app.register(appRoutes);
 app.listen({
   port: Number(PORT)
 }).then(() => console.log(`listening on port ${PORT}`));
